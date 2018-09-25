2a91900c0b9152b2a6165810a2a01a8eee71f1de
Stephan Schrijver <stephanschrijver@gmail.com>
2018-09-25 19:17:54 +0200

    values

---
+++ src/lectures/part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,18 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App {
+  val x = 42
+  println(x)
+
+  val aString: String = "Hello"
+  val anotherString = "Goodbye"
+
+  val aBoolean: Boolean = false
+  val aChar: Char = 'a'
+  val anInt: Int = x
+  val aShort: Short = 4613
+  val aLong: Long = 91240912409093113L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+  
+}
